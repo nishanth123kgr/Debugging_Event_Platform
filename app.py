4958fb450f3096526c934356eaebd50b2514d69b
Nishanth R <nishanth123kgr@gmail.com>
2024-04-06 23:44:38 +0530

    Added Sweetalert

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO
+from runcode import RunCCode
 
 
 
@@ -12,9 +13,58 @@ socketio = SocketIO (
 
 @app.route('/', methods=['GET', 'POST'])
 def show_index():
+    questions = [{
+        'question': 'Debug the code',
+        'question_desc': 'The code is not working as expected. Find the bug and fix it.',
+        'code': '#include <stdio.h>\nint main() {\n\tprintf("Hello World!");\n\treturn 0;\n}',
+        'active': True,
+        'testcases': [
+        {'num': 'Testcase 1', 'input': 'Nil', 'output': 'Hello Bey!'},
+        {'num': 'Testcase 2', 'input': 'Nil', 'output': 'Hello World!'}
+    ]
+    },
+                 {
+        'question': 'Find the output',
+        'question_desc': 'What will be the output of the following code?',
+        'code': '#include <stdio.h>\nint main() {\n\tprintf("Hello World!");\n\treturn 0;\n}',
+        'active': False,
+        'testcases': [
+        {'num': 'Testcase 1', 'input': 'Nil', 'output': 'Hello SSSS!'},
+        {'num': 'Testcase 2', 'input': 'Nil', 'output': 'Hello DDDD!'}
+    ]
+        },
+                 {
+        'question': 'Write the code',
+        'question_desc': 'Write a program to print "Hello World!"',
+        'code': 'Nil',
+        'active': False,
+        'testcases': [
+        {'num': 'Testcase 1', 'input': 'Nil', 'output': 'Thizz!'},
+        {'num': 'Testcase 2', 'input': 'Nil', 'output': 'Fizz!'}
+    ]
+        },
+                 ]
     if request.method == 'POST':
-        return {'status': 'ok'}
-    return render_template('index.html')
+        code = request.form['code']
+        run = RunCCode(code)
+        rescompil, resrun = run.run_c_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        pass
+    return render_template('index.html', questions=questions)
+
+@app.route('/submit', methods=['POST'])
+def submit_code():
+    code = request.form['code']
+    print(code)
+    run = RunCCode(code)
+    rescompil, resrun = run.run_c_code()
+    print(resrun)
+    status = {'error': 0, 'output': resrun}
+    if not resrun:
+        status = {'error': 1, 'err_desc':'Compilation Error'}
+    return jsonify({'result': status})
 
 @socketio.on('message')
 def handle_message(message):
@@ -24,4 +74,4 @@ def handle_message(message):
 
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True, port=5000)
+    socketio.run(app, allow_unsafe_werkzeug=True, port=5000, debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,76 @@
 let editor1 = CodeMirror.fromTextArea(document.getElementById("code1"), {
-    lineNumbers: true
-  });
+  lineNumbers: true
+});
 
-  let editor2 = CodeMirror.fromTextArea(document.getElementById("code2"), {
-    lineNumbers: true
-  });
+let editor2 = CodeMirror.fromTextArea(document.getElementById("code2"), {
+  lineNumbers: true
+});
 
-  let editor3 = CodeMirror.fromTextArea(document.getElementById("code3"), {
-    lineNumbers: true
-  });
+let editor3 = CodeMirror.fromTextArea(document.getElementById("code3"), {
+  lineNumbers: true
+});
 
-  let q1_btn = document.getElementById("q1_btn");
-  let q2_btn = document.getElementById("q2_btn");
-  let q3_btn = document.getElementById("q3_btn");
+let q1_btn = document.getElementById("q1_btn");
+let q2_btn = document.getElementById("q2_btn");
+let q3_btn = document.getElementById("q3_btn");
 
-function toggle_editor(qn_num){
+function toggle_editor(qn_num) {
   document.querySelector('.question-container.active').classList.remove('active');
-  document.querySelector('#q'+qn_num).classList.add('active');
-  
+  document.querySelector('#q' + qn_num).classList.add('active');
 }
 
-function toggle_qn_btn(qn_num){
+function toggle_qn_btn(qn_num) {
   document.querySelector('.question_tab.active').classList.remove('active');
-  document.querySelector('#q'+qn_num+'_btn').classList.add('active');
+  document.querySelector('#q' + qn_num + '_btn').classList.add('active');
 }
-  q1_btn.addEventListener("click", function() {
-    toggle_editor(1);
-    toggle_qn_btn(1);
-  }
-  );
+q1_btn.addEventListener("click", function () {
+  toggle_editor(1);
+  toggle_qn_btn(1);
+}
+);
 
-  q2_btn.addEventListener("click", function() {
-    toggle_editor(2);
-    toggle_qn_btn(2);
-  });
+q2_btn.addEventListener("click", function () {
+  toggle_editor(2);
+  toggle_qn_btn(2);
+});
 
-  q3_btn.addEventListener("click", function() {
-    toggle_editor(3);
-    toggle_qn_btn(3);
-  });
+q3_btn.addEventListener("click", function () {
+  toggle_editor(3);
+  toggle_qn_btn(3);
+});
+
+
+let q1_submit = document.getElementById("q1_submit");
+let q2_submit = document.getElementById("q2_submit");
+let q3_submit = document.getElementById("q3_submit");
+
+q1_submit.addEventListener("click", function () {
+  let code = editor1.getValue();
+  let formData = new FormData();
+  formData.append("code", code);
+  formData.append("qn_num", 1);
+  formData.append("submitted_time", new Date().toLocaleString());
+  formData.append("submitted_by", "user1");
+  fetch('/submit', {
+    method: 'POST',
+    body: formData
+  }).then(response => response.json())
+    .then(data => {
+      if (data.result.error == 0) {
+        Swal.fire({
+          title: "Submitted Successfully!",
+          text: "All Testcases Passesd",
+          icon: "success"
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: data.result.err_desc,
+        });
+      }
+    });
+}
+);
+
+  
