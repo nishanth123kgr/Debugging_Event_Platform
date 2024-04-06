8bd1d2e360784c44c03e274a53f5b4bf72799599
Nishanth R <nishanth123kgr@gmail.com>
2024-04-06 14:02:38 +0530

    Cleared Bugs

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,12 @@
 let editor1 = CodeMirror.fromTextArea(document.getElementById("code1"), {
-    value: `#include <stdio.h>\nint main() {\n// printf() displays the string inside quotation\nprintf("Hello, World!");\nreturn 0;\n}`,
     lineNumbers: true
   });
 
   let editor2 = CodeMirror.fromTextArea(document.getElementById("code2"), {
-    value: `#include <stdio.h>\nint main() {\n// printf() displays the string inside quotation\nprintf("Hello, World!");\nreturn 0;\n}`,
-    mode: "text/x-csrc", // Specify the mode as "text/x-csrc" for C code
     lineNumbers: true
   });
 
   let editor3 = CodeMirror.fromTextArea(document.getElementById("code3"), {
-    value: `#include <stdio.h>\nint main() {\n// printf() displays the string inside quotation\nprintf("Hello, World!");\nreturn 0;\n}`,
-    mode: "text/x-csrc", // Specify the mode as "text/x-csrc" for C code
     lineNumbers: true
   });
 
@@ -22,23 +17,25 @@ let editor1 = CodeMirror.fromTextArea(document.getElementById("code1"), {
 function toggle_editor(qn_num){
   document.querySelector('.question-container.active').classList.remove('active');
   document.querySelector('#q'+qn_num).classList.add('active');
+  
 }
 
+function toggle_qn_btn(qn_num){
+  document.querySelector('.question_tab.active').classList.remove('active');
+  document.querySelector('#q'+qn_num+'_btn').classList.add('active');
+}
   q1_btn.addEventListener("click", function() {
-    let code = editor1.getValue();
-    console.log(code);
     toggle_editor(1);
+    toggle_qn_btn(1);
   }
   );
 
   q2_btn.addEventListener("click", function() {
-    let code = editor2.getValue();
-    console.log(code);
     toggle_editor(2);
+    toggle_qn_btn(2);
   });
 
   q3_btn.addEventListener("click", function() {
-    let code = editor3.getValue();
-    console.log(code);
     toggle_editor(3);
+    toggle_qn_btn(3);
   });
