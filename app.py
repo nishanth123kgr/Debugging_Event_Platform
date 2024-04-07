3ad684937bb3d4f645633a44d154e665a5e36f9c
Nishanth R <nishanth123kgr@gmail.com>
2024-04-07 17:36:43 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, url_for, redirect
 from flask_socketio import SocketIO
 from runcode import RunCCode
+import json
+from time import sleep
+import mysql.connector
+
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'database': 'debugging',
+    'auth_plugin' : 'mysql_native_password'
+}
+
+
+qn_points = [10, 10, 20, 20, 30]
+
 
 
 
@@ -10,9 +25,26 @@ socketio = SocketIO (
       async_mode="threading"
  )
 
+app.secret_key = '875dee07a28e825074bff0e1b7da9564e107c4e3e5b809cb'
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def show_index():
+    
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    print(session['username'])
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT name, q1_status, q2_status, q3_status, q4_status from users WHERE username = %s', (session['username'],))
+    user_details = cursor.fetchall()[0]
+    user_details['username'] = session['username']
+    cursor.reset()
+    conn.close()
+    
+    print(user_details)
     questions = [{
         'question': 'Debug the code',
         'question_desc': 'The code is not working as expected. Find the bug and fix it.',
@@ -43,7 +75,35 @@ def show_index():
         {'num': 'Testcase 2', 'input': 'Nil', 'output': 'Fizz!'}
     ]
         },
+                 {
+        'question': 'Write the code',
+        'question_desc': 'Write a program to print "Hello World!"',
+        'code': 'Nil',
+        'active': False,
+        'testcases': [
+        {'num': 'Testcase 1', 'input': 'Nil', 'output': 'Thizz!'},
+        {'num': 'Testcase 2', 'input': 'Nil', 'output': 'Fizz!'}
+    ]
+        },
+                 {
+        'question': 'Find the output',
+        'question_desc': 'What will be the output of the following code?',
+        'code': '#include <stdio.h>\nint main() {\n\tprintf("Hello World!");\n\treturn 0;\n}',
+        'active': False,
+        'testcases': [
+        {'num': 'Testcase 1', 'input': 'Nil', 'output': 'Hello SSSS!'},
+        {'num': 'Testcase 2', 'input': 'Nil', 'output': 'Hello DDDD!'}
+    ]
+        }
                  ]
+    
+    codes = json.dumps([
+    "#include <stdio.h>\\nint main() {\\n\\tprintf(\\\"Hello World!\\\");\\n\\treturn 0;\\n}",
+    "#include <stdio.h>\\nint main() {\\n\\tprintf(\\\"Hello World!\\\");\\n\\treturn 0;\\n}",
+    "Nil",
+    "Nil",
+    "#include <stdio.h>\\nint main() {\\n\\tprintf(\\\"Hello World!\\\");\\n\\treturn 0;\\n}",
+    ])
     if request.method == 'POST':
         code = request.form['code']
         run = RunCCode(code)
@@ -52,11 +112,15 @@ def show_index():
             resrun = 'No result!'
     else:
         pass
-    return render_template('index.html', questions=questions)
+    return render_template('index.html', user=user_details, questions=questions, codes=codes)
 
 @app.route('/submit', methods=['POST'])
 def submit_code():
     code = request.form['code']
+    qn = request.form['qn_num']
+    submitted_time = request.form['submitted_time']
+    time_taken = json.loads(request.form['time'])
+    print(time_taken, submitted_time, qn)
     print(code)
     run = RunCCode(code)
     rescompil, resrun = run.run_c_code()
@@ -64,11 +128,67 @@ def submit_code():
     status = {'error': 0, 'output': resrun}
     if not resrun:
         status = {'error': 1, 'err_desc':'Compilation Error'}
+        return jsonify({'result': status})
+    
+    if time_taken['minutes'] < 10:
+        rate = 4
+    elif time_taken['minutes'] < 20:
+        rate = 3
+    elif time_taken['minutes'] < 30:
+        rate = 2
+    else:
+        rate = 1
+        
+    score = rate * qn_points[int(qn)-1]
+    time_taken = f"{time_taken['minutes']}:{time_taken['seconds']}"
+    
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(f'update users set q{qn}_status = 1 where username = {session["username"]}')
+    cursor.reset()
+    conn.commit()
+    query = (f"insert into q{qn} values ('{session['username']}', '{submitted_time}', '{time_taken}', {score}, '{code}')")
+    
+    print(query)
+    cursor.execute(query)
+    cursor.reset()
+    conn.commit()
+    print(query)
+    conn.close()
+    
     return jsonify({'result': status})
 
 @socketio.on('message')
 def handle_message(message):
     print(message)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        user = cursor.fetchone()
+        cursor.reset()
+        conn.close()
+        if user:
+            if password == user['password']:
+                session['username'] = user['username']
+                return redirect(url_for('show_index'))
+
+        return 'Invalid username or password'
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('show_index'))
+
 
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -10,9 +10,64 @@ let editor3 = CodeMirror.fromTextArea(document.getElementById("code3"), {
   lineNumbers: true
 });
 
+let editor4 = CodeMirror.fromTextArea(document.getElementById("code4"), {
+  lineNumbers: true
+});
+
+let editor5 = CodeMirror.fromTextArea(document.getElementById("code5"), {
+  lineNumbers: true
+});
+
+let editor_array = [editor1, editor2, editor3, editor4, editor5];
+
+let qn_timer_status = [true, false, false, false, false];
+
 let q1_btn = document.getElementById("q1_btn");
 let q2_btn = document.getElementById("q2_btn");
 let q3_btn = document.getElementById("q3_btn");
+let q4_btn = document.getElementById("q4_btn");
+let q5_btn = document.getElementById("q5_btn");
+
+let q1_reset = document.getElementById("q1_reset");
+let q2_reset = document.getElementById("q2_reset");
+let q3_reset = document.getElementById("q3_reset");
+let q4_reset = document.getElementById("q4_reset");
+let q5_reset = document.getElementById("q5_reset");
+
+let q1_timer = new easytimer.Timer();
+let q2_timer = new easytimer.Timer();
+let q3_timer = new easytimer.Timer();
+let q4_timer = new easytimer.Timer();
+let q5_timer = new easytimer.Timer();
+
+let timer_array = [q1_timer, q2_timer, q3_timer, q4_timer, q5_timer];
+
+if (localStorage.getItem(`#q1_timer`)) {
+  q1_timer.start({ startValues: JSON.parse(localStorage.getItem(`#q1_timer`)) });
+} else {
+  q1_timer.start({ precision: 'seconds', target: { hours: 1 } });
+}
+
+
+
+timer_array.forEach((timer, index) => {
+  timer.addEventListener('secondsUpdated', function (e) {
+    let time = timer.getTimeValues();
+    document.querySelector(`#q${index + 1}_timer`).innerHTML = time.toString();
+    localStorage.setItem(`#q${index + 1}_timer`, JSON.stringify(time));
+
+  });
+
+  q2_timer.addEventListener('targetAchieved', function (e) {
+    document.querySelector(`#q${index + 1}_timer`).innerHTML = 'Time UP!!';
+  });
+
+  if (localStorage.getItem(`#q${index + 1}_timer`)) {
+    timer.start({ startValues: JSON.parse(localStorage.getItem(`#q${index + 1}_timer`)) });
+  }
+});
+
+
 
 function toggle_editor(qn_num) {
   document.querySelector('.question-container.active').classList.remove('active');
@@ -24,53 +79,303 @@ function toggle_qn_btn(qn_num) {
   document.querySelector('#q' + qn_num + '_btn').classList.add('active');
 }
 q1_btn.addEventListener("click", function () {
+  if (qn_timer_status[0] == false) {
+    qn_timer_status[0] = true;
+    q1_timer.start({ precision: 'seconds', target: { hours: 1 } });
+  }
   toggle_editor(1);
   toggle_qn_btn(1);
-}
-);
+});
 
 q2_btn.addEventListener("click", function () {
+  if (qn_timer_status[1] == false) {
+    qn_timer_status[1] = true;
+    q2_timer.start({ precision: 'seconds', target: { hours: 1 } });
+  }
   toggle_editor(2);
   toggle_qn_btn(2);
 });
 
 q3_btn.addEventListener("click", function () {
+  if (qn_timer_status[2] == false) {
+    qn_timer_status[2] = true;
+    q3_timer.start({ precision: 'seconds', target: { hours: 1 } });
+  }
   toggle_editor(3);
   toggle_qn_btn(3);
+});
+
+q4_btn.addEventListener("click", function () {
+  if (qn_timer_status[3] == false) {
+    qn_timer_status[3] = true;
+    q4_timer.start({ precision: 'seconds', target: { hours: 1 } });
+  }
+  toggle_editor(4);
+  toggle_qn_btn(4);
+});
+
+q5_btn.addEventListener("click", function () {
+  if (qn_timer_status[4] == false) {
+    qn_timer_status[4] = true;
+    q5_timer.start({ precision: 'seconds', target: { hours: 1 } });
+  }
+  toggle_editor(5);
+  toggle_qn_btn(5);
 });
 
 
 let q1_submit = document.getElementById("q1_submit");
 let q2_submit = document.getElementById("q2_submit");
 let q3_submit = document.getElementById("q3_submit");
+let q4_submit = document.getElementById("q4_submit");
+let q5_submit = document.getElementById("q5_submit");
 
-q1_submit.addEventListener("click", function () {
-  let code = editor1.getValue();
-  let formData = new FormData();
-  formData.append("code", code);
-  formData.append("qn_num", 1);
-  formData.append("submitted_time", new Date().toLocaleString());
-  formData.append("submitted_by", "user1");
-  fetch('/submit', {
-    method: 'POST',
-    body: formData
-  }).then(response => response.json())
-    .then(data => {
-      if (data.result.error == 0) {
-        Swal.fire({
-          title: "Submitted Successfully!",
-          text: "All Testcases Passesd",
-          icon: "success"
-        });
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: data.result.err_desc,
-        });
-      }
-    });
+let submit_btns = [q1_submit, q2_submit, q3_submit, q4_submit, q5_submit];
+
+submit_btns.forEach((btn, index) => {
+  btn.addEventListener("click", function () {
+    let code = editor_array[index].getValue();
+    let formData = new FormData();
+    formData.append("code", code);
+    formData.append("qn_num", index + 1);
+    formData.append("submitted_time", new Date().toLocaleString());
+    formData.append("time", JSON.stringify(timer_array[index].getTimeValues()));
+    btn.querySelector('#spinner').style = 'display: inline-block;'
+    fetch('/submit', {
+      method: 'POST',
+      body: formData
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result.error == 0) {
+          Swal.fire({
+            title: "Submitted Successfully!",
+            text: `All Testcases Passesd. Solved Question ${index + 1} !! 🥳🎉`,
+            icon: "success"
+          });
+          document.querySelector(`#q${index + 1}_solved`).style = 'display: inline-flex;'
+
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: data.result.err_desc,
+          });
+        }
+        btn.querySelector('#spinner').style = 'display: none;'
+      });
+  });
 }
 );
 
-  
+
+
+// q1_submit.addEventListener("click", function () {
+//   let code = editor1.getValue();
+//   let formData = new FormData();
+//   formData.append("code", code);
+//   formData.append("qn_num", 1);
+//   formData.append("submitted_time", new Date().toLocaleString());
+//   formData.append("submitted_by", "user1");
+//   q1_submit.querySelector('#spinner').style = 'display: inline-block;'
+//   fetch('/submit', {
+//     method: 'POST',
+//     body: formData
+//   }).then(response => response.json())
+//     .then(data => {
+//       if (data.result.error == 0) {
+//         Swal.fire({
+//           title: "Submitted Successfully!",
+//           text: "All Testcases Passesd. Solved 1st Question! 🥳🎉",
+//           icon: "success"
+//         });
+//         document.querySelector('#q1_solved').style = 'display: inline-flex;'
+
+//       } else {
+//         Swal.fire({
+//           icon: "error",
+//           title: "Oops...",
+//           text: data.result.err_desc,
+//         });
+//       }
+//       q1_submit.querySelector('#spinner').style = 'display: none;'
+//     });
+// }
+// );
+
+
+// q2_submit.addEventListener("click", function () {
+//   let code = editor2.getValue();
+//   let formData = new FormData();
+//   formData.append("code", code);
+//   formData.append("qn_num", 2);
+//   formData.append("submitted_time", new Date().toLocaleString());
+//   formData.append("submitted_by", "user1");
+//   q2_submit.querySelector('#spinner').style = 'display: inline-block;'
+//   fetch('/submit', {
+//     method: 'POST',
+//     body: formData
+//   }).then(response => response.json())
+//     .then(data => {
+//       if (data.result.error == 0) {
+//         Swal.fire({
+//           title: "Submitted Successfully!",
+//           text: "All Testcases Passesd. Solved 2nd Question! 🥳🎉",
+//           icon: "success"
+//         });
+//         document.querySelector('#q2_solved').style = 'display: inline-flex;'
+
+//       } else {
+//         Swal.fire({
+//           icon: "error",
+//           title: "Oops...",
+//           text: data.result.err_desc,
+//         });
+//       }
+//       q2_submit.querySelector('#spinner').style = 'display: none;'
+
+//     });
+// });
+
+// q3_submit.addEventListener("click", function () {
+//   let code = editor3.getValue();
+//   let formData = new FormData();
+//   formData.append("code", code);
+//   formData.append("qn_num", 3);
+//   formData.append("submitted_time", new Date().toLocaleString());
+//   formData.append("submitted_by", "user1");
+//   q3_submit.querySelector('#spinner').style = 'display: inline-block;'
+//   fetch('/submit', {
+//     method: 'POST',
+//     body: formData
+//   }).then(response => response.json())
+//     .then(data => {
+//       if (data.result.error == 0) {
+//         Swal.fire({
+//           title: "Submitted Successfully!",
+//           text: "All Testcases Passesd. Solved 3rd Question! 🥳🎉",
+//           icon: "success"
+//         });
+//         document.querySelector('#q3_solved').style = 'display: inline-flex;'
+//       } else {
+//         Swal.fire({
+//           icon: "error",
+//           title: "Oops",
+//           text: data.result.err_desc,
+//         });
+//       }
+//       q3_submit.querySelector('#spinner').style = 'display: none;'
+
+//     });
+// });
+
+
+// q4_submit.addEventListener("click", function () {
+//   let code = editor4.getValue();
+//   let formData = new FormData();
+//   formData.append("code", code);
+//   formData.append("qn_num", 4);
+//   formData.append("submitted_time", new Date().toLocaleString());
+//   formData.append("submitted_by", "user1");
+//   q4_submit.querySelector('#spinner').style = 'display: inline-block;'
+//   fetch('/submit', {
+//     method: 'POST',
+//     body: formData
+//   }).then(response => response.json())
+//     .then(data => {
+//       if (data.result.error == 0) {
+//         Swal.fire({
+//           title: "Submitted Successfully!",
+//           text: "All Testcases Passesd. Solved 4th Question! 🥳🎉",
+//           icon: "success"
+//         });
+//         document.querySelector('#q4_solved').style = 'display: inline-flex;'
+
+//       } else {
+//         Swal.fire({
+//           icon: "error",
+//           title: "Oops",
+//           text: data.result.err_desc,
+//         });
+//       }
+//       q4_submit.querySelector('#spinner').style = 'display: none;'
+//     });
+// }
+// );
+
+// q5_submit.addEventListener("click", function () {
+//   let code = editor5.getValue();
+//   let formData = new FormData();
+//   formData.append("code", code);
+//   formData.append("qn_num", 5);
+//   formData.append("submitted_time", new Date().toLocaleString());
+//   formData.append("submitted_by", "user1");
+//   q5_submit.querySelector('#spinner').style = 'display: inline-block;'
+//   fetch('/submit', {
+//     method: 'POST',
+//     body: formData
+//   }).then(response => response.json())
+//     .then(data => {
+//       if (data.result.error == 0) {
+//         Swal.fire({
+//           title: "Submitted Successfully!",
+//           text: "All Testcases Passesd. Solved 5th Question! 🥳🎉",
+//           icon: "success"
+//         });
+//         document.querySelector('#q5_solved').style = 'display: inline-flex;'
+
+//       } else {
+//         Swal.fire({
+//           icon: "error",
+//           title: "Oops",
+//           text: data.result.err_desc,
+//         });
+//       }
+//       q5_submit.querySelector('#spinner').style = 'display: none;'
+//     });
+// }
+// );
+
+q1_reset.addEventListener("click", function () {
+  editor1.setValue(default_codes[0]);
+});
+
+q2_reset.addEventListener("click", function () {
+  editor2.setValue(default_codes[1]);
+});
+
+q3_reset.addEventListener("click", function () {
+  editor3.setValue(default_codes[2]);
+});
+
+q4_reset.addEventListener("click", function () {
+  editor4.setValue(default_codes[3]);
+});
+
+q5_reset.addEventListener("click", function () {
+  editor5.setValue(default_codes[4]);
+});
+
+
+var timer = new easytimer.Timer();
+if (localStorage.getItem('time')) {
+  timer.start({ countdown: true, startValues: JSON.parse(localStorage.getItem('time')) });
+} else {
+  timer.start({ countdown: true, startValues: { hours: 1 } });
+}
+
+document.querySelector('#timer').innerHTML = timer.getTimeValues().toString();
+
+timer.addEventListener('secondsUpdated', function (e) {
+  let time = timer.getTimeValues();
+  document.querySelector('#timer').innerHTML = time.toString();
+  localStorage.setItem('time', JSON.stringify(time));
+
+});
+
+timer.addEventListener('targetAchieved', function (e) {
+  document.querySelector('#timer').innerHTML = 'Time UP!!';
+});
+
+
+
+
